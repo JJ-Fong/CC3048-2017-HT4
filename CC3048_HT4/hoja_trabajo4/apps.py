e03bd18c4413c2318e155966f1810fd83ee92e4b
JJ-Fong <jjosuefong@gmail.com>
2017-08-23 11:17:34 -0600

    app set up

---
+++ CC3048_HT4/hoja_trabajo4/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class HojaTrabajo4Config(AppConfig):
+    name = 'hoja_trabajo4'
---
+++ ven/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Javier Fong\Documents\Universidad\CICLO 10\Tecnologias Integradoras\HT4\New folder\ven\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
