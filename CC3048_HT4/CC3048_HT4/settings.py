4aabec1e8ceeb4a9f09c80a5001c416692dff9d8
JJ-Fong <jjosuefong@gmail.com>
2017-08-23 12:19:44 -0600

    db set up

--- CC3048_HT4/CC3048_HT4/settings.py
+++ CC3048_HT4/CC3048_HT4/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hoja_trabajo4.apps.HojaTrabajo4Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- CC3048_HT4/hoja_trabajo4/models.py
+++ CC3048_HT4/hoja_trabajo4/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+	id = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 20)
+ 	precio = models.FloatField(null = False, blank = False)
+	
+class Ingrediente(models.Model):
+	id = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 20)
+	costo = models.FloatField(null = False, blank = False)
+
+class Receta(models.Model):
+	id_prod = models.ForeignKey(Producto, on_delete = models.CASCADE)
+	id_ingrediente = models.ForeignKey(Ingrediente, on_delete = models.CASCADE)
+	
